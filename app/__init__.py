ac01f152996c432e80345fcdb9721619d0c756c6
Omar Najera <omar.navzz@gmail.com>
2021-10-27 01:02:48 -0500

    Se crea la aplicacion

---
+++ app/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+app.config.from_object('config.DevelopmentConfig')
+
+URL = 'https://api.spacexdata.com/v4/rockets'
+
+@app.route('/')
+def index():
+    data = requests.get(URL)
+    data = data.json()
+    return render_template('index.html',data=data)
